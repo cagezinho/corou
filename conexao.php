43e6a2d3f48442e31ee0a16084509c8cbe41a26d
Nicolas Cage <116232150+cagezinho@users.noreply.github.com>
2024-08-24 22:40:12 -0300

    a

---
+++ conexao.php
@@ -0,0 +1,15 @@
+<?php 
+
+    $host = '';
+    $user = 'nicol530_main';
+    $pass = 'Ooa^FVukFbqK(gdB%O';
+    $db = 'nicol530_main';
+
+    $mysqli = new mysqli($host, $user, $pass, $db);
+
+    $conexao = mysqli_connect($host, $user, $pass, $db);
+
+    if($mysqli->error) {
+        die("Falha ao conectar ao banco de dados: " . $mysqli->error);
+    }
+?>
---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+
+    <title>Corou!</title>
+</head>
+<body>
+    <nav class="header">
+        <h1>Quantas vezes a COROU pediu pra muudar de equipe</h1>
+    </nav>
+    <main>
+        <div class="sidebar">
+            <div class="sidebar-card">
+                <form action="" method="post">
+                    <!-- preciso da data pra salvar no banco -->
+                </form>
+            </div>
+        </div>
+        <section>
+
+        </section>
+    </main>
+
+</body>
+</html>
