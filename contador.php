809131885da49bd585934ba1e91e291415313f6f
Nicolas Cage <116232150+cagezinho@users.noreply.github.com>
2024-08-24 22:55:11 -0300

    d

--- contador.php
+++ contador.php
@@ -3,7 +3,9 @@
 
     $data = mysqli_real_escape_string($conexao, $_POST['timestamp']);
 
-    $inserir_data = "INSERT INTO corou (timestamp_coluna) VALUES ('$data')";
+    $inserir_data = "INSERT INTO
+    corou
+    VALUE ('', '$date')";
 
     $query_users = mysqli_query($conexao, $inserir_data);
 
